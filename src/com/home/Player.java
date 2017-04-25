0de90d6ba5c4463c05ad44e12e98c9e91b472f15
Brett Marcinkiewicz <brettmarcin@gmail.com>
2017-04-24 20:50:20 -0400

    added player class

---
+++ src/com/home/Player.java
@@ -0,0 +1,15 @@
+package com.home;
+
+public class Player {
+	public String first;
+	public String last;
+	public String pos;
+	public String team;
+	public double Fpoints;
+	public int passYards;
+	public int passTDs;
+	public int ints;
+	public int rushYards;
+	public int rushTDs;
+	public int fum;
+}
